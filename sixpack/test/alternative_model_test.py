b27ea5e4d36d6d14f4882a6dde2cfd189c74be3c
zackkitzmiller <zackkitzmiller@gmail.com>
2013-01-18 12:21:31 -0500

    tests: alternative model tests

--- sixpack/test/alternative_model_test.py
+++ sixpack/test/alternative_model_test.py
@@ -1,4 +1,8 @@
+
 import unittest
+from numbers import Number
+from mock import MagicMock
+from sixpack.db import REDIS
 
 from sixpack.models import Alternative
 
@@ -6,8 +10,11 @@ class TestAlternativeModel(unittest.TestCase):
 
     unit = True
 
+    def setUp(self):
+        self.redis = MagicMock(REDIS)
+
     def test_key(self):
-        alt = Alternative('yes', 'show-something')
+        alt = Alternative('yes', 'show-something', self.redis)
         key = alt.key()
         self.assertEqual(key, 'sixpack:show-something:yes')
 
@@ -21,5 +28,51 @@ class TestAlternativeModel(unittest.TestCase):
         unicode_valid = Alternative.is_valid(u'valid')
         self.assertTrue(unicode_valid)
 
+    def test_reset(self):
+        alt = Alternative('yes', 'show-something', self.redis)
+        alt.reset()
+        self.redis.hset.assert_any_call(alt.key(), 'participant_count', 0)
+        self.redis.hset.assert_any_call(alt.key(), 'completed_count', 0)
+
+    def test_delete(self):
+        alt = Alternative('yes', 'show-something', self.redis)
+        alt.delete()
+        self.redis.delete.assert_called_once_with(alt.key())
+
+    def test_is_control(self):
+        pass
+
+    def test_experiment(self):
+        pass
+
+    def test_participant_count(self):
+        self.redis.hget.return_value = 1
+
+        alt = Alternative('yes', 'show-something', self.redis)
+        count = alt.participant_count()
+
+        self.redis.hget.assert_called_once_with(alt.key(), 'participant_count')
+        self.assertTrue(isinstance(count, Number))
+
+        self.redis.reset_mock()
+
+    def test_completion_count(self):
+        self.redis.hget.return_value = 1
+
+        alt = Alternative('yes', 'show-something', self.redis)
+        count = alt.completed_count()
+
+        self.redis.hget.assert_called_once_with(alt.key(), 'completed_count')
+        self.assertTrue(isinstance(count, Number))
+
+        self.redis.reset_mock()
+
+    def test_increment_participation(self):
+        alt = Alternative('yes', 'show-something', self.redis)
+        alt.increment_participation()
+        self.redis.hincrby.assert_called_once_with(alt.key(), 'participant_count', 1)
+
     def test_increment_completion(self):
-        self.assertTrue(True)
+        alt = Alternative('yes', 'show-something', self.redis)
+        alt.increment_completion()
+        self.redis.hincrby.assert_called_once_with(alt.key(), 'completed_count', 1)
