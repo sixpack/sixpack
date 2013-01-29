7537c61b7c1251b7ae30d0def54c8d8e9052a8bb
zackkitzmiller <zackkitzmiller@gmail.com>
2013-01-29 09:41:54 -0500

    stubbing out sixpack web controllers

---
+++ sixpack/sixpack-web.py
@@ -0,0 +1,46 @@
+from flask import Flask
+import db
+
+app = Flask(__name__)
+
+# List of experiments
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+# Details for experiment
+@app.route("/experiment/<experiment_name>/")
+def details(experiment_name):
+    pass
+
+# Set winner for an experiment
+@app.route("/experiment/<experiment_name>/winner/", methods=['POST'])
+def set_winner(experiment_name):
+    pass
+
+# Reset experiment
+@app.route("/experiment/<experiment_name>/reset/", methods=['POST'])
+def reset_experiment(experiment_name):
+    pass
+
+# Reset experiment winner
+@app.route("/experiment/<experiment_name>/winner/reset/", methods=['POST'])
+def reset_winner(experiment_name):
+    pass
+
+# Delete experiment
+@app.route("/experiment/<experiment_name>/delete/", methods=['DELETE'])
+def delete_experiment(experiment_name):
+    pass
+
+# Archive experiment
+@app.route("/experiment/<experiment_name>/archive", methods=['POST'])
+def archive_experiment(archive_experiment):
+    pass
+
+@app.route('/favicon.ico')
+def favicon():
+    return ''
+
+if __name__ == "__main__":
+    app.run(port=5051, debug=True)
