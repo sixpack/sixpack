de64d415e84268f2a3fc0186dda5e4ad88176da0
zackkitzmiller <zackkitzmiller@gmail.com>
2013-02-05 08:24:07 -0500

    minor: small script to load data and convert.

--- seed.py
+++ seed.py
@@ -13,14 +13,30 @@ experiments = {
 
 # generate 20 client ids
 client_ids = []
-for i in range(1, 20):
+for i in range(1, 800):
     client_ids.append(''.join(random.choice(string.ascii_lowercase + string.digits + '-_') for x in range(30)))
 
+client_exp = []
 participations = {}
 for x in range(1, 10):
     for experiment, alternatives in experiments.iteritems():
         _id = random.choice(client_ids)
+
+        # no dupes, plz
+        client_ids.remove(_id)
+
+        client_exp.append((_id, experiment))
+
         payload = {'experiment': experiment, 'alternatives': alternatives, 'client_id': _id}
         ret = requests.get('http://localhost:5000/participate', params=payload)
         print ret.headers
 
+for key, tup in enumerate(client_exp):
+    if key % 2:
+        client_id, exp = tup
+        payload = {
+            'experiment': exp,
+            'client_id': client_id
+        }
+        ret = requests.get('http://localhost:5000/convert', params=payload)
+        print ret.headers
