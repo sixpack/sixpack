18892bd9d7a98998ad2ee1e5d0a639dc9381f4f5
Mike Dirolf <mike@dirolf.com>
2013-02-05 12:28:42 -0500

    tests that don't pass yet

--- sixpack/test/server_test.py
+++ sixpack/test/server_test.py
@@ -4,17 +4,30 @@ import fakeredis
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
 
-from sixpack.server import Sixpack
+from sixpack.server import (create_app,
+                            Sixpack)
 
 
 class TestServer(unittest.TestCase):
 
     def setUp(self):
-        self.redis = fakeredis.FakeStrictRedis()
-        self.app = Sixpack(self.redis)
+        # tried using fakeredis here but it barfed on scripts
+        self.app = create_app()
         self.client = Client(self.app, BaseResponse)
 
-    def base_test(self):
+    def test_base(self):
         self.assertEqual(200, self.client.get("/").status_code)
 
+    def test_sans_callback(self):
+        res = self.client.get("/participate?experiment=dummy&client_id=foo&alternatives=one&alternatives=two")
+        self.assertEqual(200, res.status_code)
+        self.assertEqual("application/json", dict(res.headers)["Content-Type"])
+        self.assert_(res.data.startswith("{"))
+        self.assert_(res.data.endswith("}"))
 
+    def test_with_callback(self):
+        res = self.client.get("/participate?experiment=dummy&client_id=foo&alternatives=one&alternatives=two&callback=seatgeek.cb")
+        self.assertEqual(200, res.status_code)
+        self.assertEqual("application/javascript", dict(res.headers)["Content-Type"])
+        self.assert_(res.data.startswith("foo({"))
+        self.assert_(res.data.endswith("})"))
