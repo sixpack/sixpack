8d009da779e6258e32220732a754b0eba3fb5341
Jack Groetzinger <jack@seatgeek.com>
2013-02-06 17:50:48 -0500

    Working ugly d3 graph

--- sixpack/static/js/chart.js
+++ sixpack/static/js/chart.js
@@ -1,12 +1,22 @@
 $(function() {
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var arrData = [
+      ["2012-10-02",0.05],
+      ["2012-10-03",0.056],
+      ["2012-10-04",0.057],
+      ["2012-10-05",0.0575],
+      ["2012-10-06",0.059],
+      ["2012-10-07",0.0582],
+      ["2012-10-08",0.0584],
+      ["2012-10-09",0.0586]];
 
-  var parseDate = d3.time.format("%d-%b-%y").parse;
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 300
+      height = 120
+
+  var parseDate = d3.time.format("%Y-%m-%d").parse;
 
   var x = d3.time.scale()
-      .range([0, width]);
+      .range([0, width])
 
   var y = d3.scale.linear()
       .range([height, 0]);
@@ -23,39 +33,40 @@ $(function() {
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.close); });
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select(".graph").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.csv.parseRows(data, function(data) {
-    data.forEach(function(d) {
-      d.date = parseDate(d.date);
-      d.close = +d.close;
-    });
-
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.close; }));
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Price ($)");
-
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
+  var data = arrData.map(function(d) {
+      return {
+         date: parseDate(d[0]),
+         close: d[1]
+      };
   });
+
+  console.log(data);
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
 });
