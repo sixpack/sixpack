daf7b6edad542af43aebd9f15211307b926ce8ff
Jack Groetzinger <jack@seatgeek.com>
2013-02-14 09:58:46 -0500

    Closes #17

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,9 +1,18 @@
 $(function() {
-	$('#client').click(function() {
-		$('#upper').animate({
-			height: '+=85px'
-		}, function() {
-			$('#clients').fadeIn();
+	$('#client').bind('click', show_clients);
+	$('#upper-content').animate({ opacity: 1 }, {queue: false, duration: 800});
+	$('#upper-content').animate({ top: 0 }, 600, function() {
+		$('#github-links').animate({ opacity: 1 }, function() {
+			$('#byline').animate({ opacity: 1 });
 		});
 	});
 });
+
+function show_clients() {
+	$('#upper').animate({
+		height: '+=65px'
+	}, function() {
+		$('#clients').fadeIn();
+	});
+	$('#client').unbind('click', show_clients);
+}
