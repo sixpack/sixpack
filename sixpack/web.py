5fc97337717adf4fad4a7de42bee39431f27f529
zackkitzmiller <zackkitzmiller@gmail.com>
2013-03-08 13:07:33 -0500

    move error templates, add 500 error page

--- sixpack/web.py
+++ sixpack/web.py
@@ -102,7 +102,13 @@ def favicon():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
 
 def find_or_404(experiment_name):
     try:
