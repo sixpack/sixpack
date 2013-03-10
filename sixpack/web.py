ad481677099d1a73a752471729d931aaa11a031e
Matthew Hudson <matt@matthewghudson.com>
2013-03-10 15:34:23 -0400

    Added $.waypoints plugin.

--- sixpack/web.py
+++ sixpack/web.py
@@ -14,6 +14,7 @@ csrf = SeaSurf(app)
 js = Bundle('js/jquery.js', 'js/d3.js',
             'js/bootstrap.min.js', 'js/chart.js',
             'js/script.js', 'js/underscore-min.js',
+            'js/waypoints.min.js',
             output="{0}/sixpack.js".format(cfg.get('asset_path', 'gen')))
 
 css = Bundle('css/bootstrap.css',
