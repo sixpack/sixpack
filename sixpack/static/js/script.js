59373e9d12e19d2e5275a578ea0aa2fd2a01fd71
Matthew Hudson <matt@matthewghudson.com>
2013-03-11 19:02:13 -0400

    Rewrite how Dashboard data is rendered.

--- sixpack/static/js/script.js
+++ sixpack/static/js/script.js
@@ -7,29 +7,26 @@ $(function () {
     var path = document.location.pathname;
     var experiment_name = path.slice(12, path.length - 1);
 
-    var experiment = new Experiment(experiment_name);
+    var experiment = new Experiment($('ul.experiments'), experiment_name);
   }
-
 
   // Draw charts on Dashboard page.
   if ($('#dashboard-page').length) { 
+    _.each(experiments, function (experiment_name) {
+      $('ul.experiments').append('<li data-name="' + experiment_name + '" style="visibility: hidden;"></li>')
+    });
+
     $('li').waypoint(function (direction) {
-      var el = $(this).find('.chart');
+      var el = $(this);
+      var experiment_name = el.data('name');
 
       // Prevent loading more than once:
       if (el.data('loaded')) return;
       el.data('loaded', true);
-
-      var colors = []; 
-      var circles = el.parent().find('span.circle').get();
-      _.each(circles, function (val, index) {
-        colors.push($(circles[index]).css('stroke'));
-      });
-      var experiment_name = el.data('experiment');
-      var chart = new Chart(experiment_name, function () {
-        chart.drawExperiment(experiment_name, colors);
-        el.fadeIn();
-      });
+      
+      var experiment = new Experiment(el, experiment_name);
+      
+      el.css('visibility', 'visible');
     }, {
       offset: 'bottom-in-view'
     });
