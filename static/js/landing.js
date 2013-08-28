ef23f15926159b68fac48cebe0e14928c6705671
Jack Groetzinger <jack@seatgeek.com>
2013-08-27 20:13:31 -0400

    Include web UI more info section

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,5 +1,6 @@
 $(function() {
 	$('#client').bind('click', show_clients);
+  $('#see-more-web').bind('click', show_web_ui);
 	$('#upper-content').animate({ opacity: 1 }, {queue: false, duration: 800});
 	$('#upper-content').animate({ top: 0 }, 700, 'easeOutQuad', function() {
 		$('#github-links').animate({ opacity: 1 }, function() {
@@ -8,13 +9,28 @@ $(function() {
 		});
 	});
 	var s = skrollr.init();
+  $('.tooltip-marker').tooltip();
 });
 
 function show_clients() {
 	$('#upper').animate({
-		height: '+=65px'
+		paddingBottom: '+=65px'
 	}, function() {
 		$('#clients').fadeIn();
 	});
 	$('#client').unbind('click', show_clients);
+}
+
+function show_web_ui(e) {
+  e.preventDefault();
+  $('#feature-depth').show();
+  $('#feature-depth').animate({
+    minHeight: '+=1143px'
+  }, function() {
+    $('#sixpack-web-large, #feature-depth h4').animate({ opacity: 1 }, 600, function() {
+        $('#see-more-web').fadeOut();
+      }
+    );
+  });
+  $('#see-more-web').unbind('click', show_web_ui);
 }
