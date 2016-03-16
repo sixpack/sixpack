6b3e5f4c980828bcce906b3ae2e36e4dbe777d79
Igor Bondarenko <jetmind2@gmail.com>
2016-03-16 18:07:03 +0200

    Add tests for metrics

--- sixpack/metrics.py
+++ sixpack/metrics.py
@@ -1,8 +1,8 @@
-import statsd
+from statsd import StatsClient
 
 
 def init_statsd(config):
     host = config.get('statsd_host', 'localhost')
     port = config.get('statsd_port', 8125)
     prefix = config.get('statsd_prefix', 'sixpack')
-    return statsd.StatsClient(host, port, prefix=prefix)
+    return StatsClient(host, port, prefix=prefix)
